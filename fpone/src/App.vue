94b8419b7cb91a27143d39d24818bc5bd89d5d65
kihhmaster <kihhmaster@yandex.ru>
2021-07-10 18:49:10 +0300

    Lesson_1 finished

---
+++ fpone/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+		<Calc/>
+    <HelloWorld 
+		:msg="massage" 
+		:myArr="arr"/>
+		Text : {{ massage }}  длина {{ massage.length }} 
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+		Calc
+  },
+	data: () =>({
+		massage: "Ваша реклама",
+		arr: [1,2,3,4,5]
+	})
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ fpone/src/components/Calc1.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+
+		<div>
+			<input type="number" placeholder="op1" v-model.number="operand1">
+			<input placeholder="op2" v-model.number.lazy="operand2"/>
+			={{ sum }}
+		</div>
+		<div>
+			<button @click="sum = operand1+operand2">+</button>
+			<button v-on:click="sum = operand1-operand2">-</button>
+			<button @click="sum = operand1 * operand2">*</button>
+			<button  @click="div">/</button>
+			<button class="step"  @click="sum = Math.pow(operand1, operand2)">степень</button>
+			<button class="step"  @click="sum = parseInt(operand1/operand2)">Целочисленное деление</button>
+			<button @click="eventFv">Event</button>
+		</div>
+
+		result: {{ sum }}
+		<!-- <div v-on:mouseover=""></div> -->
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Calc",
+	data: () => ({
+		operand1: 0,
+		operand2: 0,
+		sum: 0
+
+	}),
+	methods: {
+
+		eventFv() {
+			console.log(arguments)
+		},
+		div(){
+			this.sum = this.operand1 / this.operand2
+		}
+	},
+}
+</script>
+
+<style lang="scss">
+
+</style>
