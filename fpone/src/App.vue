b411108586f2825c704610481be717584c7f3d7f
kihhmaster <kihhmaster@yandex.ru>
2021-07-15 17:06:38 +0300

    cleaning the application

--- fpone/src/App.vue
+++ fpone/src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-		<Calc/>
-    <HelloWorld 
-		:msg="massage" 
-		:myArr="arr"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Calc from './components/Calc1.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-		Calc
+
   },
 	data: () =>({
-		massage: "Ваша реклама",
-		arr: [1,2,3,4,5]
+
 	})
 }
 </script>
