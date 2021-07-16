d4475deffb21de57a89e26d876efefe9e1a06955
kihhmaster <kihhmaster@yandex.ru>
2021-07-16 16:34:06 +0300

    Finished lesson_3

--- fpone/src/App.vue
+++ fpone/src/App.vue
@@ -1,25 +1,65 @@
 <template>
-  <div id="app">
-
+  <div id="app" :class="[$style.wrapper]">
+    <header>
+      <h1>My personal cost</h1>
+    </header>
+		
+    <main>
+			<AddPayment @addNewPayment="addData"/>
+      <PaymentsDisplay :list="paymentsList" />
+    </main>
   </div>
 </template>
 
 <script>
-
-
+import PaymentsDisplay from "./components/PaymentsDisplay.vue";
+import AddPayment from "./components/AddPayment.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
+    PaymentsDisplay,
+		AddPayment,
   },
-	data: () =>({
-
-	})
-}
+  data: () => ({
+    paymentsList: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  }),
+  methods: {
+		addData (data){
+			this.paymentsList.push(data)
+		},
+    fetchData() {
+      return [
+        {
+          date: "28.03.2020",
+          category: "Food",
+          value: 169,
+        },
+        {
+          date: "28.03.2020",
+          category: "Sport",
+          value: 1690,
+        },
+        {
+          date: "28.03.2020",
+          category: "Car",
+          value: 900,
+        },
+        {
+          date: "28.03.2020",
+          category: "Family",
+          value: 9000,
+        },
+      ];
+    },
+  },
+	created() {
+		this.paymentsList = this.fetchData()
+	},
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" module>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +67,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.wrapper {
+  display: flex;
+	flex-direction: column;
 }
 </style>
---
+++ fpone/src/components/PaymentsDisplay.vue
@@ -0,0 +1,74 @@
+<template>
+	<div class="payments-list">
+		<div class="wrapp-list__title">
+			<div class="list__title">#</div>
+			<div class="list__title">Date</div>
+			<div class="list__title">Category</div>
+			<div class="list__title">Value</div>
+		</div>
+		<div class="wrapp-list__item" v-for="(item, idx) in list" :key="idx">
+			<div class="list__item">{{ idx + 1 }}</div>
+			<div class="list__item">{{ item.date}}</div>
+			<div class="list__item">{{ item.category}}</div>
+			<div class="list__item">{{ item.value}}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "PaymentsDisplay",
+	props: {
+		list: {
+			type: Array,
+			default: ()=>[]
+		}
+	},
+	data() {
+		return {
+
+		}
+	}
+}
+</script>
+
+
+<style lang="scss" scoped>
+.payments-list {
+	width: 100%;
+	max-width: 600px;
+}
+.wrapp-list__item {
+	display: flex;
+	justify-content: flex-start;
+	align-items: flex-start;
+	flex-direction: row;
+	border-bottom: 1px solid black;
+	width: 100%;
+	&:nth-child(even) {
+		background: rgb(250, 249, 249);
+	}
+
+}
+.wrapp-list__title {
+	width: 100%;
+	display: flex;
+	justify-content: flex-start;
+	align-items: flex-start;
+	flex-direction: row;
+	border-bottom: 1px solid black;
+}
+
+.list__title {
+	width: 150px;
+	margin: 10px;
+	font-size: 18px;
+	font-weight: bold;
+}
+.list__item {
+	width: 150px;
+	margin: 10px;
+
+}
+
+</style>
