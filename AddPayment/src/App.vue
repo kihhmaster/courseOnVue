76b845a0f0ef598e9d7f6787cb3bf13bfb5068d7
kihhmaster <kihhmaster@yandex.ru>
2021-07-29 12:47:55 +0300

    lesson_5--hash

--- AddPayment/src/App.vue
+++ AddPayment/src/App.vue
@@ -3,14 +3,22 @@
     <header>
       <h1>My personal cost</h1>
     </header>
-		
+    <div class="menu">
+      <a href="#dashboard">Dashboard / </a>
+      <a href="#about">About / </a>
+      <a href="#notFound">Not Found / </a>
+    </div>
     <main>
-			<Pagination />
-			<AddPayment @addNewPayment="addData"/>
-			Total: {{ getFPV }}
-			<CategorySelect :category="category" />
+			<div class="content-page">
+				<About v-if="page === 'about'"/>
+				<Dashboard v-if="page === 'dashboard'"/>
+				<NotFound v-if="page === 'notFound'"/>
+			</div>
+      <Pagination />
+      <AddPayment @addNewPayment="addData" />
+      Total: {{ getFPV }}
+      <CategorySelect :category="category" />
       <PaymentsDisplay :list="paymentsList" />
-      
     </main>
   </div>
 </template>
@@ -19,105 +27,95 @@
 import PaymentsDisplay from "./components/PaymentsDisplay.vue";
 import CategorySelect from "./components/CategorySelect.vue";
 import Pagination from "./components/pagination.vue";
+
+import About from "./views/About.vue";
+import Dashboard from "./views/Dashboard.vue";
+import NotFound from "./views/NotFound.vue";
+
 import AddPayment from "./components/AddPayment.vue";
-import { mapMutations, mapGetters, mapActions} from "vuex";
+import { mapMutations, mapGetters, mapActions } from "vuex";
 
 export default {
+
   name: "App",
   components: {
     PaymentsDisplay,
-		AddPayment,
-		CategorySelect,
-		Pagination
+    AddPayment,
+    CategorySelect,
+    Pagination,
+		About,
+		Dashboard,
+		NotFound,
   },
-  methods: {
-		...mapMutations([
-			'setPaymentListData',
-			'addDataToPaymentsList'
-		]),
-		...mapActions([
-			'fetchData',
-			'fetchCategory'
-		]),
-		addData (data){
-			// this.paymentsList.push(data)
-			// this.paymentsList = [...this.paymentsList, data]
-			this.addDataToPaymentsList(data)
-		},
-    /*fetchData() {
-      return [
-        {
-          date: "28.03.2020",
-          category: "Food",
-          value: 169,
-        },
-        {
-          date: "28.03.2020",
-          category: "Sport",
-          value: 1690,
-        },
-        {
-          date: "28.03.2020",
-          category: "Car",
-          value: 900,
-        },
-        {
-          date: "28.03.2020",
-          category: "Family",
-          value: 9000,
-        },
-      ];
-    },*/
-
-  },
-	computed: {
-		...mapGetters({
-			paymentsList: 'getPaymentList',
-			category: 'getCategoryList'
-		}),
-		getFPV() {
-			return this.$store.getters.getFullPaymentValue
-		},
-		// paymentsList() {
-		// 	return this.$store.getters.getPaymentList
-		// }
-	},
-	created() {
-		// this.$store.commit('setPaymentListData', this.fetchData())
-		//можно и так , только после спред оператора строка 29
-		// this.setPaymentListData(this.fetchData()) 
-		// this.paymentsList = this.fetchData()
-		//имитация сервера
-		this.$store.dispatch('fetchData')
-		//Подмешиваем метод строка 32
-		this.fetchData()
-		if(!this.category.length) {
-			this.fetchCategory()
+	data() {
+		return {
+			page: ''
 		}
-		
+	},
+  methods: {
+    ...mapMutations(["setPaymentListData", "addDataToPaymentsList"]),
+    ...mapActions(["fetchData", "fetchCategory"]),
+    addData(data) {
+      // this.paymentsList.push(data)
+      // this.paymentsList = [...this.paymentsList, data]
+      this.addDataToPaymentsList(data);
+    },
+		setPage() {
+			this.page = location.hash.slice(1)
+		},
+  },
+  computed: {
+    ...mapGetters({
+      paymentsList: "getPaymentList",
+      category: "getCategoryList",
+    }),
+    getFPV() {
+      return this.$store.getters.getFullPaymentValue;
+    },
+    // paymentsList() {
+    // 	return this.$store.getters.getPaymentList
+    // }
+  },
+  created() {
+    // this.$store.commit('setPaymentListData', this.fetchData())
+    //можно и так , только после спред оператора строка 29
+    // this.setPaymentListData(this.fetchData())
+    // this.paymentsList = this.fetchData()
+    //имитация сервера
+    this.$store.dispatch("fetchData");
+    //Подмешиваем метод строка 32
+    this.fetchData();
+    if (!this.category.length) {
+      this.fetchCategory();
+    }
+  },
+	mounted() {
+		this.setPage()
+		window.addEventListener('hashchange', ()=>{
+			this.setPage()
+		})
 	},
 };
 </script>
 
 <style lang="scss" module>
-
 .wrapper {
   display: flex;
-	flex-direction: column;
+  flex-direction: column;
 }
 </style>
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Otomanopee+One&display=swap');
-	#app {
-		font-family: 'Otomanopee One', sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		font-size: 18px;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+@import url("https://fonts.googleapis.com/css2?family=Otomanopee+One&display=swap");
+#app {
+  font-family: "Otomanopee One", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 18px;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
